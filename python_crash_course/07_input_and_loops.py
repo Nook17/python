4dc7c50d8144cb48e9df9f72e57a32ead7510535
homeLinux <arek@nook17.pl>
2021-08-02 17:26:30 +0200

    07 input and loops - Done

--- python_crash_course/07_input_and_loops.py
+++ python_crash_course/07_input_and_loops.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def keep_going():
-    print('\nKeep going !!!')
+    print('\nKeep going !!!\n')
 
 prompt = 'You can Stay or Exit the program [S/e]: '
 choose = input(prompt) or 's'
@@ -11,4 +11,19 @@ elif choose == 'e':
     exit()
 else:
     print('fuck off')
+
+# --- move list items ---
+old_list = ['kojo', 'kama', 'nook', 'amon', 'nook', 'fiolka', 'dosia', 'nook']
+new_list = []
+
+while old_list:
+    current_item = old_list.pop()
+    new_list.append(current_item)
+print(f"New list -> {new_list}")
+
+# --- remove 'nook' item form new_list[] ---
+while 'nook' in new_list:
+    new_list.remove('nook')
+print(f"List without 'nook' -> {new_list}")
+
 
