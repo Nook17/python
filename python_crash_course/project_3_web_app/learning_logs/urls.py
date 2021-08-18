4cf03cf7b11d940b217709da975407d27ee3c8e9
workLinux <arek@nook17.pl>
2021-08-18 14:30:16 +0200

    project web page 532

--- python_crash_course/project_3_web_app/learning_logs/urls.py
+++ python_crash_course/project_3_web_app/learning_logs/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
-    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
-]
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    path('update_entry/<int:entry_id>/', views.update_entry, name='update_entry'),
+    path('delete_entry/<int:entry_id>/', views.delete_entry, name='delete_entry'),
+    ]
--- python_crash_course/project_3_web_app/learning_logs/views.py
+++ python_crash_course/project_3_web_app/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -40,4 +40,31 @@ def new_entry(request, topic_id):
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
     context = {'topic': topic, 'form': form}
-    return render(request, 'learning_logs/new_entry')
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def update_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':    # if 'GET'
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/update_entry.html', context)
+
+def delete_entry(request, entry_id):
+    try:
+        entry = Entry.objects.get(id=entry_id)
+        topic = Topic.objects.get(id=entry.topic.id)
+        entries = topic.entry_set.order_by('-date_added')
+    except Entry.DoesNotExist:
+        return redirect('learning_logs:index')
+    entry.delete()
+
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
