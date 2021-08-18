9f78ddfb3d635bc2cf23e1a7d8e6007d4ab8bc4e
homeLinux <arek@nook17.pl>
2021-08-18 21:50:39 +0200

    Django_Udemy - Academind - up to 26 lesson

--- django/02_academind/monthly_challenges/challenges/urls.py
+++ django/02_academind/monthly_challenges/challenges/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'challenges'
 urlpatterns = [
-        path('', views.index),
-        path('january/', views.january),
+        path('', views.index, name='index'),
+        # path('january/', views.january, name='january'),
+        # path('february/', views.february, name='february'),
+        path('<nook>', views.nook_func),
         ]
--- django/02_academind/monthly_challenges/challenges/views.py
+++ django/02_academind/monthly_challenges/challenges/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
     return render(request, 'challenges/index.html')
 
-def january(request):
-    return render(request, 'challenges/january.html')
+# def january(request):
+#     return render(request, 'challenges/january.html')
+
+# def february(request):
+#     return render(request, 'challenges/february.html')
+
+def nook_func(request, nook_arg):
+    if nook_arg == january:
+        set_path = 'challenges/january.html'
+    elif nook_arg == february:
+        set_path = 'challenges/february.html'
+    elif nook_arg == march:
+        set_path = 'challenges/march'
+    else:
+        return HttpResponseNotFound(request, 404)
+    return render(request, set_path)
+        
