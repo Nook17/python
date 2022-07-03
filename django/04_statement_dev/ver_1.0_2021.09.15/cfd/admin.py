b685aad2d2bbe7137e37b599c091392acaa13076
homeLinux <arek@nook17.pl>
2022-07-03 21:40:55 +0200

    bookr_03.07.2022

--- django/04_statement_dev/ver_1.0_2021.09.15/cfd/admin.py
+++ django/04_statement_dev/ver_1.0_2021.09.15/cfd/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from cfd.models import Statement, Deposit, Withdrawal, Notesdb, Buy_calc, Quarter, Pipmargin
+
+admin.site.register(Statement)
+admin.site.register(Deposit)
+admin.site.register(Withdrawal)
+admin.site.register(Notesdb)
+admin.site.register(Buy_calc)
+admin.site.register(Quarter)
+admin.site.register(Pipmargin)
