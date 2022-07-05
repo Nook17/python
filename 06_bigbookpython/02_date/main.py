065022ab59af973844a425d27130d94393c8495d
workLinux <arek@nook17.pl>
2022-07-05 15:06:08 +0200

    05.07.2022

---
+++ 06_bigbookpython/02_date/main.py
@@ -0,0 +1,13 @@
+import datetime, random
+
+
+def getBirthdays(numberOfBirthdays):
+    birthdays = []
+    for i in range(numberOfBirthdays):
+        first_dat = datetime.date(1980, 1, 1)
+        dat_delta = datetime.timedelta(random.randint(0, 364))
+        dat_sum = first_dat + dat_delta
+        birthdays.append(dat_sum)
+    return birthdays
+
+print(" ".join(str(getBirthdays(10))))
