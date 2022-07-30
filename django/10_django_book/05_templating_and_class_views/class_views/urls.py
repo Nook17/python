2929b86f750b51be4f7680118074e494961ade31
homeLinux <arek@nook17.pl>
2022-07-30 11:26:05 +0200

    10_django_book

--- django/10_django_book/05_templating_and_class_views/class_views/urls.py
+++ django/10_django_book/05_templating_and_class_views/class_views/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import BookRecordFormView, FormSuccessView
+from .views import BookRecordFormView, FormSuccessView, BookCreateView, BookUpdateViews, BookDeleteViews, \
+    FormSuccessDelete, BookRecordDetailView
 
 urlpatterns = [
     path('new_book_record', BookRecordFormView.as_view(), name='book_record_form'),
-    path('entry_success', FormSuccessView.as_view(), name='form_success')
+    path('entry_success', FormSuccessView.as_view(), name='form_success'),
+    path('book_record_create', BookCreateView.as_view(), name='book_create'),
+    path('book_record_update/<int:pk>', BookUpdateViews.as_view(), name='book_update'),
+    path('book_record_delete/<int:pk>', BookDeleteViews.as_view(), name='book_delete'),
+    path('delete_success', FormSuccessDelete.as_view(), name='form_delete_success'),
+    path('book_record_detail/<int:pk>', BookRecordDetailView.as_view(), name='book_detail'),
 ]
--- django/10_django_book/05_templating_and_class_views/class_views/views.py
+++ django/10_django_book/05_templating_and_class_views/class_views/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
 from django.views import View
-
+from .models import Book
 from .forms import BookForm
 
 
@@ -15,6 +15,39 @@ class BookRecordFormView(FormView):
         form.save()
         return super().form_valid(form)
 
+
 class FormSuccessView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("model records have been saved")
+
+
+# --------- CRUD ---------
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['name', 'author']
+    template_name = 'book_form.html'
+    success_url = '/class_views/entry_success'
+
+
+class BookUpdateViews(UpdateView):
+    model = Book
+    fields = ['name', 'author']
+    template_name = 'book_form.html'
+    success_url = '/class_views/entry_success'
+
+
+class BookDeleteViews(DeleteView):
+    model = Book
+    fields = ['name', 'author']
+    template_name = 'book_delete_form.html'
+    success_url = '/class_views/delete_success'
+
+
+class FormSuccessDelete(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Your book has been deleted")
+
+
+class BookRecordDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
